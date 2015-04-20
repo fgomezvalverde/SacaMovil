8da729ac51ef1f95e6954ea44bf0b1644565d3d3
fgomezvalverde <fafa_gttm@hotmail.com>
2015-04-19 19:41:01 -0600

    Agregando al List de COmisiones

--- SACA-AE/www/js/index_user_scripts.js
+++ SACA-AE/www/js/index_user_scripts.js
@@ -11,7 +11,6 @@
      /* button  Login */
     $(document).on("click", ".uib_w_10", function(evt)
     {
-        $(":mobile-pagecontainer").pagecontainer("change", "#selectionsub", { reverse: false});
                     
         var urlLogin = "http://localhost:50297/WebServiceMobile.asmx?op=Login";
 
@@ -36,6 +35,52 @@
     
      
      //document.getElementById("txt_correo").value
+        /* button  #but_prueba */
+    
+        /* button  #but_prueba */
+    
+    
+        /* button  #but_prueba */
+    $(document).on("click", "#but_prueba", function(evt)
+    {
+         activate_subpage("#selectionsub"); 
+    });
+    
+        
+    
+    
+    
+        /* boton de Comisiones en Menu Principal*/
+    $(document).on("click", ".uib_w_5", function(evt)
+    {
+        
+        
+        
+        
+        var urlGetComisiones = "http://localhost:50297/WebServiceMobile.asmx?op=GetComisiones";
+
+        var soapMessageGetComisiones =
+            '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"> \
+            <soap:Body> \
+            <GetComisiones xmlns="http://saca-ae.net/"> \
+            <pMail>' + document.getElementById("txt_correo").value.toString() + '</pMail> \
+            </GetComisiones> \
+            </soap:Body> \
+            </soap:Envelope>';
+        $.ajax({
+            url: urlGetComisiones,
+            type: "POST",
+            dataType: "xml",
+            data: soapMessageGetComisiones,
+            complete: endComisiones,
+            contentType: "text/xml; charset=\"utf-8\""
+            });
+        
+        
+        
+        
+    });
+    
     }
  document.addEventListener("app.Ready", register_event_handlers, false);
     
@@ -53,13 +98,51 @@
                 
                 if(name == "1")
                 {
-                    alert("Usuario Existe");
+                    
+                    activate_subpage("#selectionsub"); 
                     
                 }
                 else
                 {
-                    alert("Usuario NO Existe");
+                    alert("Usuario NO Existe "+document.getElementById("txt_correo").value.toString());
                 }
+            }
+    
+    function endComisiones(xmlHttpRequest, status)
+            {
+                
+                var nombre,id,profesor,inicio,fin,entidad;
+                var list_html = "";
+                var panel_content = "";
+             $(xmlHttpRequest.responseXML)
+                .find('GetComisionesResult')
+                .each(function()
+             {
+                 alert($(this).find('AlertaComisionProfesor').text());
+                 
+                 
+                nombre= $(this).find('COMISION').text();
+                 id= $(this).find('ID').text();
+                 profesor= $(this).find('PROFESOR').text();
+                 inicio= $(this).find('INICIO').text();
+                 fin= $(this).find('FIN').text();
+                 entidad= $(this).find('ENTIDAD').text();
+                 
+                 
+                 
+                 panel_content = '<h2>'+nombre+'<p>Profesor: '+profesor+'</p><p>Inicio: '+inicio+'</p><p>Fin: '+fin+'</p><p>Entidad: '+entidad+'</p>';
+            $.ui.addContentDiv(id, panel_content, "Movie Info"); 
+                 
+                 list_html += '<li><a href="#'+id+'">'+ nombre +'</a></li>';
+             });
+                
+                
+        
+         
+        
+                
+                $("#list_comisiones5").append(list_html);
+                activate_page("#list_page");
             }
 
     
